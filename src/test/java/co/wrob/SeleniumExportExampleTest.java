ba974b514f514197c064796829fea4d0e0c8bf4b
Justin Wrobel <justin.wrobel@laureate.net>
2017-08-15 17:09:18 -0400

    initial commit

---
+++ src/test/java/co/wrob/SeleniumExportExampleTest.java
@@ -0,0 +1,95 @@
+package co.wrob;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumExportExampleTest {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver-mac-64bit");
+		driver = new FirefoxDriver();
+		baseUrl = "https://www.google.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void example1() throws Exception {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.id("lst-ib")).clear();
+		driver.findElement(By.id("lst-ib")).sendKeys("Hello worldasdasd");
+		driver.findElement(By.cssSelector("input.lsb")).click();
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.id("lst-ib")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
+
+		driver.findElement(By.id("lst-ib")).clear();
+		driver.findElement(By.id("lst-ib")).sendKeys("Hello world");
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
---
+++ src/test/java/co/wrob/WebDriverExtensionsExampleTest.java
@@ -0,0 +1,69 @@
+package co.wrob;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.github.webdriverextensions.Bot;
+import com.github.webdriverextensions.junitrunner.WebDriverRunner;
+import com.github.webdriverextensions.junitrunner.annotations.Chrome;
+import com.github.webdriverextensions.junitrunner.annotations.DriverPaths;
+import com.github.webdriverextensions.junitrunner.annotations.Firefox;
+import com.github.webdriverextensions.junitrunner.annotations.ScreenshotsPath;
+import com.github.webdriverextensions.junitrunner.annotations.TakeScreenshotOnFailure;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(WebDriverRunner.class)
+@Chrome
+@Firefox
+@TakeScreenshotOnFailure
+@ScreenshotsPath("target/screenshots")
+//@DriverPaths(firefox="drivers/geckodriver-mac-64bit", chrome="drivers/chromedriver-mac-64bit")
+public class WebDriverExtensionsExampleTest {
+
+    @FindBy(xpath="//input[@id='lst-ib']")
+    WebElement queryInput;
+    @FindBy(name = "btnK")
+    WebElement searchButton;
+    @FindBy(id = "ires")
+    WebElement searchResult;
+
+    @Test
+    public void searchGoogleForHelloWorldTest() {
+    	
+    	log.warn("!!! {}", "hi");
+    	Bot.open("http://www.google.com");
+        Bot.assertCurrentUrlContains("google");
+        Bot.waitForElementToDisplay(queryInput);
+        log.debug("wait {}", queryInput.getAttribute("name"));
+        
+        Bot.type("Hello World", queryInput);
+        Bot.click(searchButton);
+
+        Bot.waitFor(1, SECONDS);
+        log.warn("!!! {}", searchResult.getAttribute("data-async-context"));
+        Bot.assertAttributeContains("data-async-context", "query:Hello%20World", searchResult);
+    }
+    
+    
+    @Test
+    public void searchGoogleForHelloWorldTestShouldFail() {
+    	
+    	log.warn("!!! {}", "hi");
+    	Bot.open("http://www.google.com");
+    	Bot.assertCurrentUrlContains("google");
+    	Bot.waitForElementToDisplay(queryInput);
+        log.debug("wait {}", queryInput.getAttribute("name"));
+        
+        Bot.type("Hello World", queryInput);
+        Bot.click(searchButton);
+
+        Bot.waitFor(1, SECONDS);
+        log.warn("!!! {}", searchResult.getAttribute("data-async-context"));
+        Bot.assertAttributeContains("data-async-context", "query:!!!!", searchResult);
+    }
+}
